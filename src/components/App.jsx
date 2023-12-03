b564374de56d7892e97913e5a396b4b155571759
rudopulos <79414851+rudopulos@users.noreply.github.com>
2023-12-03 12:56:54 +0200

    hw4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,51 @@
-export const App = () => {
+import React, { useState, useEffect } from 'react';
+import ContactForm from './ContactForm';
+import Filter from './Filter';
+import ContactList from './ContactList';
+
+const App = () => {
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    const storedContacts = JSON.parse(localStorage.getItem('contacts')) || [];
+    setContacts(storedContacts);
+  }, []);
+
+  const addContact = (newContact) => {
+    const isDuplicate = contacts.some((contact) => contact.name.toLowerCase() === newContact.name.toLowerCase());
+  
+    if (isDuplicate) {
+      alert(`${newContact.name} is already in the contacts.`);
+      return;
+    }
+  
+    setContacts((prevContacts) => {
+      const updatedContacts = [...prevContacts, newContact];
+      localStorage.setItem('contacts', JSON.stringify(updatedContacts));
+      return updatedContacts;
+    });
+  };
+
+  const deleteContact = (id) => {
+    const updatedContacts = contacts.filter((contact) => contact.id !== id);
+    setContacts(updatedContacts);
+    localStorage.setItem('contacts', JSON.stringify(updatedContacts));
+  };
+
+  const filteredContacts = contacts.filter((contact) =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm contacts={contacts} onAddContact={addContact} />
+      <h2>Contacts</h2>
+      <Filter value={filter} onChange={(e) => setFilter(e.target.value)} />
+      <ContactList contacts={filteredContacts} onDelete={deleteContact} />
     </div>
   );
 };
+
+export default App;
